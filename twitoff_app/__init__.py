bed3b17711cfa800ccf2f9a0b3cb6e25d26f3024
noreallyimfine <avrahamjacobsohn@gmail.com>
2019-09-17 18:12:28 -0400

    create db and confirm connect to twitter api

--- twitoff_app/__init__.py
+++ twitoff_app/__init__.py
@@ -1,10 +1,13 @@
 """Entry point for TwitOff"""
+from decouple import config
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///twitoff.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = config("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['ENV'] = config('ENV')
 DB = SQLAlchemy(app)
 
 from twitoff_app import routes
