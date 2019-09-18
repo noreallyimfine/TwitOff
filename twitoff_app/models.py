b0dbaf5769a03ac4c32e8fa572251fc6ba02b7b6
noreallyimfine <avrahamjacobsohn@gmail.com>
2019-09-18 18:50:20 -0400

    built in predict, added some style

--- twitoff_app/models.py
+++ twitoff_app/models.py
@@ -2,18 +2,22 @@ from twitoff_app import DB
 
 
 class User(DB.Model):
-    id = DB.Column(DB.Integer, primary_key=True)
-    username = DB.Column(DB.String(20), unique=True, nullable=False)
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    name = DB.Column(DB.String(20), unique=True, nullable=False)
+    newest_tweet_id = DB.Column(DB.BigInteger)
 
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f'<User {self.name}>'
 
 
 class Tweet(DB.Model):
-    id = DB.Column(DB.Integer, primary_key=True)
-    tweet = DB.Column(DB.Unicode(280))
-    user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    text = DB.Column(DB.Unicode(500))
+    embedding = DB.Column(DB.PickleType, nullable=False)
+    user_id = DB.Column(DB.BigInteger,
+                        DB.ForeignKey('user.id'),
+                        nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
 
     def __repr__(self):
-        return f'<Tweet {self.tweet}>'
+        return f'<Tweet {self.text}>'
--- twitoff_app/routes.py
+++ twitoff_app/routes.py
@@ -1,15 +1,62 @@
-from flask import render_template
+from flask import render_template, request
 from twitoff_app import app, DB
 from .models import User
+from .twitter import add_or_update_user
+from .predict import predict_user
 
 
 # route determines location
 @app.route('/')
 def home():
     users = User.query.all()
-    return render_template('home.html', users=users)
+    return render_template('home.html', title='Home', users=users)
+
+
+@app.route('/user', methods=['POST'])
+@app.route('/user/<name>', methods=['GET'])
+def user(name=None):
+    message = ''
+    name = name or request.values['user_name']
+    try:
+        if request.method == 'POST':
+            add_or_update_user(name)
+            message = f'User {name} successfully added!'
+        tweets = User.query.filter(User.name == name).one().tweets
+    except Exception as e:
+        message = f'Error adding {name}: {e}'
+        tweets = []
+    return render_template('user.html', title='User', tweets=tweets,
+                           message=message)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    users = User.query.all()
+    return render_template('about.html', title='About', users=users)
+
+
+@app.route('/reset')
+def reset():
+    DB.drop_all()
+    DB.create_all()
+    return render_template('home.html', title='Reset', users=[])
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    user1 = request.values['user1']
+    user2 = request.values['user2']
+    tweet_text = request.values['tweet_text']
+    users = User.query.all()
+    usernames = [user.name for user in users]
+    if user1 not in usernames:
+        raise ValueError('User 1 is not in the app. Please add user first')
+    elif user2 not in usernames:
+        raise ValueError('User 2 is not in the app. Please add user first')
+    else:
+        pred = predict_user(user1, user2, tweet_text)[0]
+        if pred == 0:
+            message = f'{user2} is more likely to tweet this than {user1}'
+        else:
+            message = f'{user1} is more likely to tweet this than {user2}'
+        return message
