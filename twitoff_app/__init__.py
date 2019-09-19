8078a1af053d5c7672bf086215379477aee247f5
noreallyimfine <avrahamjacobsohn@gmail.com>
2019-09-19 16:24:02 -0400

    still tinking

--- twitoff_app/__init__.py
+++ twitoff_app/__init__.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = config("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['ENV'] = config('ENV')
+# app.config['ENV'] = config('ENV')
 DB = SQLAlchemy(app)
 
 from twitoff_app import routes
